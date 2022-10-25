b9253fe7ebd061a286c05458dc16b59038983859
Mohamed Mazlin <64913677+MohaMAZ@users.noreply.github.com>
2022-10-26 00:26:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p> """
+    return 'Hello heroku'
 
 if __name__ == '__main__':
     app.run()
